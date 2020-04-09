0374fb114a9be3ebce8dd6d3dee310bb2a788b21
carolbarata <carol.barata@hotmail.com>
2020-04-09 15:31:46 +0100

    data extracting accommodates genome/xchr level assemblies

--- data_extracting_submissionscript.sh
+++ data_extracting_submissionscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -V ## pass all environment variables to the job, VERY IMPORTANT
-#$ -N snp_extract_g ## job name
+#$ -N snp_extract_112 ## job name
 #$ -S /bin/bash ## shell where it will run this job
 #$ -j y ## join error output to normal output
 #$ -cwd ## Execute the job from the current working directory
@@ -15,19 +15,36 @@ conda activate bcftools_env
 ##### Set main variables ####
 BCFTOOLSDIR=/shelf/apps/cdcbrb/conda/envs/bcftools_env/bin/
 BGZIPDIR=/shelf/apps/cdcbrb/conda/envs/bcftools_env/bin/
-ALIGNDIR=/storage/home/users/cdcbrb/dpseudo_data/new_ref_mapping/
+ALIGNDIR=/storage/home/users/cdcbrb/dpseudo_data/new_ref_mapping/xchr/
 SCRIPTDIR=/storage/home/users/cdcbrb/dpseudo_mapping_pipeline/
 
 ##### Count variants with bcftools #####
-touch ${ALIGNDIR}snp_count_per_sample_bwa_both_callers.counts
-touch ${ALIGNDIR}snp_count_per_sample_novoalign_both_callers.counts
-for SAMPLE in $(find ${ALIGNDIR} -maxdepth 1 -type d -name 'G*_F');
+
+ASSEMBLYLV="xchr"
+
+if [ ${ASSEMBLYLV} = "xchr" ];
+then
+	BWACOUNTSFILE=${ALIGNDIR}snp_count_per_sample_bwa_both_callers_xchr.counts
+	NOVOCOUNTSFILE=${ALIGNDIR}snp_count_per_sample_novoalignn_both_callers_xchr.counts
+else
+	BWACOUNTSFILE=${ALIGNDIR}snp_count_per_sample_bwa_both_callers.counts
+	NOVOCOUNTSFILE=${ALIGNDIR}snp_count_per_sample_novoalign_both_callers.counts
+fi
+
+#### Hash two lines below if the 
+#### sample you're about to parse ####
+#### isn't the first ever ####
+
+#touch ${BWACOUNTSFILE}
+#touch ${NOVOCOUNTSFILE}
+
+for SAMPLE in $(find ${ALIGNDIR} -maxdepth 1 -type d -name 'G112*_F');
 do
 	SAMPLENAME=$(echo ${SAMPLE} | grep -oE '[^/]+_F')
 	BWAVCF=${SAMPLE}/*_bwa_both_callers.vcf.gz
 	NOVOALIGNVCF=${SAMPLE}/*_novoalign_both_callers.vcf.gz
-	${SCRIPTDIR}main_data_extracting_pipeline.sh -id ${SAMPLENAME}_bwa_both_callers -i ${BWAVCF} -b ${BCFTOOLSDIR} -z ${BGZIPDIR} -o ${SAMPLE} -c ${ALIGNDIR}snp_count_per_sample_bwa_both_callers.counts
-	${SCRIPTDIR}main_data_extracting_pipeline.sh -id ${SAMPLENAME}_novoalign_both_callers -i ${NOVOALIGNVCF} -b ${BCFTOOLSDIR} -z ${BGZIPDIR} -o ${SAMPLE} -c ${ALIGNDIR}snp_count_per_sample_novoalign_both_callers.counts
+	${SCRIPTDIR}main_data_extracting_pipeline.sh -id ${SAMPLENAME}_bwa_both_callers -i ${BWAVCF} -b ${BCFTOOLSDIR} -z ${BGZIPDIR} -o ${SAMPLE} -c ${BWACOUNTSFILE} -l ${ASSEMBLYLV}
+	${SCRIPTDIR}main_data_extracting_pipeline.sh -id ${SAMPLENAME}_novoalign_both_callers -i ${NOVOALIGNVCF} -b ${BCFTOOLSDIR} -z ${BGZIPDIR} -o ${SAMPLE} -c ${NOVOCOUNTSFILE} -l ${ASSEMBLYLV}
 
 done
 
--- main_data_extracting_pipeline.sh
+++ main_data_extracting_pipeline.sh
@@ -4,10 +4,9 @@ set -u
 set -o pipefail
 
 ##################################################
-# Script that uses bcftools to output a
-# multi-sample pileup file
-# NB: input bamfile should refer to a single
-# treatment (all replicates, all time points)
+# Script that uses bcftools to extract relevant
+# variant data information from VCF file and 
+# extract the total number of SNPs called
 ##################################################
 
 USAGE="$(basename "$0") [-h] [-id -b -s -fb -f -o -r -i] --
@@ -19,7 +18,8 @@ where:\n
     -b full path to bcftools directory\n
     -z full path to bgzip directory\n
     -o full path to output directory\n
-    -c full path to file containing SNP counts per sample"
+    -c full path to file containing SNP counts per sample\n
+    -l assembly level (genome/xchr)"
 
 
 while (( "$#" )); do
@@ -48,6 +48,10 @@ case "$1" in
     COUNTSFILE="$2"
     shift 2
     ;;
+    -l|--assemblylevel)
+    LEVEL="$2"
+    shift 2
+    ;;
     -h|--help)
     echo -e $USAGE
     shift 2
@@ -68,12 +72,16 @@ done
 
 ##### Extract ALT allele variant calling & genotype calling qual scores #####
 echo -e 'CHROM\tPOS\tREF\tALT\tQUAL\tGT\tDP\tADF\tADR\tAD' > ${OUTDIR}/${SAMPLEID}_snp_data.txt
-#${BCFTOOLSDIR}bcftools filter --exclude TYPE="indel" ${VCF} > ${VCF}.temp
+if [ ${LEVEL} = "genome" ];
+then
+	${BCFTOOLSDIR}bcftools filter --exclude TYPE="indel" ${VCF} > ${VCF}.temp
+fi
+
 ${BCFTOOLSDIR}bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\t%QUAL[\t%GT\t%DP\t%ADF\t%ADR\t%AD]\n' ${VCF} >> ${OUTDIR}/${SAMPLEID}_snp_data.txt
-rm ${VCF}.temp
 
 [ -s "${OUTDIR}/${SAMPLEID}.vcfstats" ] && [ -s "${OUTDIR}/${SAMPLEID}_snp_data.txt" ] && echo "Stats were computed and data also seems to have been extracted, woohoo!" || echo "Whoops, something's gone wrong with data extracting and/or stats computing."
 
 ##### Extract SNP number from stats file #####
 
 echo -e "$(echo ${SAMPLEID} | grep -oE '[^/]+_F' )\t$(grep -E "^SN.*SNPs:" ${OUTDIR}/${SAMPLEID}.vcfstats | awk -F: '{print $NF}' | sed -e 's/^[ \t]*//')" >> ${COUNTSFILE}
+
